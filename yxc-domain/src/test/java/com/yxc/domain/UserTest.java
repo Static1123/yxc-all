6cb963539f0ac745020d288250852fb74df55c10
static <705523636@qq.com>
2018-05-23 18:02:23 +0800

    First

--- yxc-domain/src/test/java/com/yxc/domain/UserTest.java
+++ yxc-domain/src/test/java/com/yxc/domain/UserTest.java
@@ -3,11 +3,20 @@ package com.yxc.domain;
 import com.yxc.domain.dao.UserDao;
 import com.yxc.domain.service.UserService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:db.xml"})
 public class UserTest {
     private static ApplicationContext context = new ClassPathXmlApplicationContext("classpath:db.xml");
+
+    @Autowired
+    private UserDao userDao;
 
     public static void main(String[] args) {
         UserService userService = (UserService) context.getBean("userService");
@@ -17,7 +26,7 @@ public class UserTest {
 
     @Test
     public void testUser() {
-        UserDao userDao = context.getBean(UserDao.class);
+//        UserDao userDao = context.getBean(UserDao.class);
         userDao.add();
     }
 }
