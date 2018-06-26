e323ea7242460b93d2849a58e1ee2eb1d9394249
static <705523636@qq.com>
2018-06-26 16:41:15 +0800

    web

--- yxc-domain/src/test/java/com/yxc/domain/UserTest.java
+++ yxc-domain/src/test/java/com/yxc/domain/UserTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 
@@ -58,5 +59,56 @@ public class UserTest {
 
         list = jdbcTemplate2.queryForList("select * from `a` limit 10");
         System.out.println(list);
+    }
+
+    @Test
+    public void testReadFile() {
+        readFileByLines("C:\\Users\\Administrator\\Desktop\\log4jd.log");
+    }
+
+
+    private static void readFileByLines(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString;
+            // 一次读入一行，直到读入null为文件结束
+            System.out.println("==================================");
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                if (tempString.contains("-thread-198")) {
+                    System.out.println(tempString);
+                    appendMethodA("bbbb.log", tempString + "\r\n");
+                }
+            }
+            System.out.println("==================================");
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+    }
+
+    private static void appendMethodA(String fileName, String content) {
+        try {
+            // 打开一个随机访问文件流，按读写方式
+            RandomAccessFile randomFile = new RandomAccessFile(fileName, "rw");
+            // 文件长度，字节数
+            long fileLength = randomFile.length();
+            //将写文件指针移到文件尾。
+            randomFile.seek(fileLength);
+            randomFile.write(content.getBytes("UTF-8"));
+            randomFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
